bf7090e27099f444d37fdd2e94000ac28155329a
Shoaib-ur-Rehman <160622745+shoaiburrehman2211@users.noreply.github.com>
2024-04-22 03:18:41 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,25 @@
+/*If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that 
+includes at least three people you’d like to invite to dinner. Then use your list to print a message to 
+each person, inviting them to dinner.
+*/
+
+
+const myFriends: string [] = ["Zohaib", "Ayaz", "Rameez","Junaid", "Faisal","Kamran"];
+
+// 1st method with loop
+
+for(let i = 0; i < myFriends.length; i++){
+console.log(`Mr. ${myFriends[i]}!you are invited to my dinner on Sunday `)
+}
+
+
+//2nd method with loop
+
+//myFriends.map((ib))=>{
+//console.log(`Mr. ${myFriends [ib]}! you are invited to my dinner on Sunday`);
+//}
+
+
+
+
+
